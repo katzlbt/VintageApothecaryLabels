c3ea62d0d92c0761a2cbcc250efbea3f981d327b
Thomas K. <thomas.katzlberger@locandy.com>
2025-07-05 20:19:55 +0200

    Use style, cleanup

--- make-labels.sh
+++ make-labels.sh
@@ -18,7 +18,7 @@ while getopts i:o:t: OPT; do
             TEMPLATE=$OPTARG
             ;;
         *)
-            echo $0 "-i input-directory (default: yamls)"
+            echo $0 "-i input-directory (default: jsons)"
             echo $0 "-o output-directory (default: svgs)"
             echo $0 "-t template.mustache (default: ApothecaryLabel.mustache)"
             ;;
@@ -35,7 +35,7 @@ shopt -u nullglob # Turn off nullglob to restore default behavior
 
 # 3. Check if any SVG files were found.
 if [ ${#JSON_FILES[@]} -eq 0 ]; then
-  echo "Error: No YAML files found in '$INPUT_DIR'."
+  echo "Error: No JSON files found in '$INPUT_DIR'."
   exit 1
 fi
 
@@ -49,21 +49,3 @@ for FP in "${JSON_FILES[@]}" ; do
     cat "$INPUT_DIR"/$FILE | sed "s|\$FONT|$FONT|g" 
     cat "$INPUT_DIR"/$FILE | sed "s|\$FONT|$FONT|g" | $MUSTACHE > "$OUTPUT_DIR"/"${FILE%.*}.svg"
 done
-
-exit 1
-
-echo '{"label":"Piper Nigrum", "sublabel":"grana integra", $FONT}' > $OUTPUT_DIR/Piper.json
-echo '{"label":"Piper Viridis", "sublabel":"grana integra", $FONT}' > $INPUT_DIR/PiperV.json
-echo '{"label":"Piper Longum", "sublabel":"fructus integra", $FONT}' > $INPUT_DIR/PiperL.json
-echo '{"label":"Pimenta Dioica", "sublabel":"grana integra", $FONT}' > $INPUT_DIR/Pimenta.json
-echo '{"label":"Folia Lauri", "sublabel":"folia integra", $FONT}' > $INPUT_DIR/Laurum.json
-echo '{"label":"Sinapis Alba", "sublabel":"grana integra", $FONT}' > $INPUT_DIR/Sinapis.json
-echo '{"label":"Capiscum Annum", "sublabel":"contriti fructus", $FONT}' > $INPUT_DIR/Chili.json
-echo '{"label1":"Schinus", "label2":"Terebinthifolia", "sublabel":"grana integra", $FONT}' > $INPUT_DIR/Schinus.json
-echo '{"label1":"Folia", "label2":"Citrus Hystix", "sublabel":"folia integra", $FONT}' > $INPUT_DIR/Hystix.json
-echo '{"label":"Acidum Citricum", "sublabel":"crystallizatum", $FONT}' > $INPUT_DIR/AcidumCitricum.json
-
-
-
-#echo '{"label":"", "sublabel":"", '$FONT'}' | mustache ApothecaryLabel.mustache > Laurum.svg
-#echo '{"label":"", "sublabel":"", '$FONT'}' | mustache ApothecaryLabel.mustache > Laurum.svg
